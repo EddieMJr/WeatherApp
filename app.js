824fce7d7a782aa953630450cbeb4c003dc6a455
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-08-29 10:53:17 -0400

    added comments to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// grabs export from geocode and forecast js pages ans imports them here
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
 const address = process.argv[2]
 
+// all code to catch errors that may come through when finding location
 if (!address) {
     console.log('please provide an address')
 } else {
