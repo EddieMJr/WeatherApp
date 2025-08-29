94cdf1ff53b421dd59c70815f3f4c29898c30e5a
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-08-29 01:28:07 -0400

    completed section 8

--- webServer/public/js/app.js
+++ webServer/public/js/app.js
@@ -1,1 +1,26 @@
 console.log('Client side js file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messOne = document.querySelector('#errorMess')
+const messTwo = document.querySelector('#location')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messOne.textContent = 'loading...'
+    messTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then ((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            messOne.textContent = data.error
+        } else {
+            messOne.textContent = data.location
+            messTwo.textContent = data.forecast
+        }
+    })
+})
+})
